5fc3e7202d23a478b8ad413d0223a3b96b2989ba
Klay Clarke <klayaclarke@gmail.com>
2022-02-12 12:48:08 -0500

    add main js file

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const createNewTODO = (project, title, description, dueDate, priority) => {
+    // create new todo in local storage db
+    // assign new todo to a project (if no project, simply to main)
+    // give todo description entered by user
+    // give todo duedate entered by user
+    // give todo priority entered by user
+    break
+};
+
+const changeTODOPriority = (todoID) => {
+    // allow user to change priority of certain todo by clicking on option symbol, and change priority option
+    // provide options for change and append changes to local storage db
+    break
+}
+
+const setTODOAsComplete = (todoID) => {
+    // allow user to set certain todo as complete by clicking on a check box
+    // store 100 most recent completed todos in local storage 
+    break
+}
+
+const createNewProject = (name) => {
+    // allow user to create new project by entering a project name
+    break
+};
+
+const addTODOToProject = (todoID, projectID) => {
+    // allow user to append a previously created todo to a project of their choosing
+    break
+}
