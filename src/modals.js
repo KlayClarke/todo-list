6506aa6280db767f14dc739a23fad411811573fd
Klay Clarke <klayaclarke@gmail.com>
2022-02-13 14:34:46 -0500

    allow user to hide modal by clicking close button

--- src/modals.js
+++ src/modals.js
@@ -1,18 +1,33 @@
 const chestIcon = document.querySelector("#chest");
 const chestModal = document.querySelector("#chest-modal");
+const trashIcon = document.querySelector("#trash");
+const trashModal = document.querySelector("#trash-modal");
+const closeButtons = document.querySelectorAll(".close-button");
 
-const closeButton = document.querySelector(".close-button");
+function windowOnClick(e) {
+  if (e.target === chestModal) {
+    toggleChestModal();
+  } else if (e.target === trashModal) {
+    toggleTrashModal();
+  }
+}
 
 function toggleChestModal() {
   chestModal.classList.toggle("show-modal");
 }
 
-function windowOnClick(e) {
-  if (e.target === chestModal) {
-    toggleChestModal();
-  }
+function toggleTrashModal() {
+  trashModal.classList.toggle("show-modal");
 }
 
+closeButtons.forEach((closeButton) => {
+  if (closeButton.classList.contains("for-chest")) {
+    closeButton.addEventListener("click", toggleChestModal);
+  } else {
+    closeButton.addEventListener("click", toggleTrashModal);
+  }
+});
+
 chestIcon.addEventListener("click", toggleChestModal);
-closeButton.addEventListener("click", toggleChestModal);
+trashIcon.addEventListener("click", toggleTrashModal);
 window.addEventListener("click", windowOnClick);
