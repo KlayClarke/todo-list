1770bbb546637607c053a5a610e8b38f05bf3528
Klay Clarke <klayaclarke@gmail.com>
2022-02-12 19:28:34 -0500

    when user enters title, descrip and clicks create button, create object with the following: a unique id (using date.now), title, description

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import { compareAsc, format } from "date-fns";
+import { storageAvailable } from "./storage";
+
+if (storageAvailable("localStorage") && !localStorage.getItem("data")) {
+  // populate data on page
+} else {
+  // create new data object and store it empty
+}
 
 document.querySelector(".create-todo").addEventListener("click", function () {
-  createNewTODO();
+  if (storageAvailable("localStorage")) createNewTODO();
 });
 
 const createNewTODO = () => {
@@ -10,27 +17,33 @@ const createNewTODO = () => {
   // give todo description entered by user
   // give todo duedate entered by user
   // give todo priority entered by user
-  const newTODO = document.createElement("div");
-  newTODO.innerHTML = `${
-    document.querySelector("input[name='title']").value
-  }<br>${document.querySelector("textarea[name='description']").value}`;
-  document.querySelector(".todos").appendChild(newTODO);
+  if (document.querySelector("input[name='title']").value) {
+    console.log("nice name");
+    let todo = {
+      id: Date.now(),
+      title: document.querySelector("input[name='title']").value,
+      description: document.querySelector("textarea[name='description']").value,
+    };
+    console.log(todo);
+  } else {
+    console.log("Please name your todo!");
+  }
 };
 
-const changeTODOPriority = (todoID) => {
-  // allow user to change priority of certain todo by clicking on option symbol, and change priority option
-  // provide options for change and append changes to local storage db
-};
+// const changeTODOPriority = (todoID) => {
+//   // allow user to change priority of certain todo by clicking on option symbol, and change priority option
+//   // provide options for change and append changes to local storage db
+// };
 
-const setTODOAsComplete = (todoID) => {
-  // allow user to set certain todo as complete by clicking on a check box
-  // store 100 most recent completed todos in local storage
-};
+// const setTODOAsComplete = (todoID) => {
+//   // allow user to set certain todo as complete by clicking on a check box
+//   // store 100 most recent completed todos in local storage
+// };
 
-const createNewProject = (name) => {
-  // allow user to create new project by entering a project name
-};
+// const createNewProject = (name) => {
+//   // allow user to create new project by entering a project name
+// };
 
-const addTODOToProject = (todoID, projectID) => {
-  // allow user to append a previously created todo to a project of their choosing
-};
+// const addTODOToProject = (todoID, projectID) => {
+//   // allow user to append a previously created todo to a project of their choosing
+// };
